87b4a48cc6e7cec59564fd2f4524d8a16e0ca755
Anastasia <vainer.lena@gmail.com>
2022-12-02 00:03:42 +0400

    done

--- Seminar001_hw1.3/Program.cs
+++ Seminar001_hw1.3/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Enter num : ");
+string enterNum = Console.ReadLine();
+
+int num = Convert.ToInt32(enterNum);
+
+if(num%2 != 0)
+    {
+        Console.WriteLine("NO");
+    }
+    else
+        {
+            Console.WriteLine("YES");
+        }
