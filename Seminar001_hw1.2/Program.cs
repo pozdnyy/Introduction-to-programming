b5bf68d0050eb98717ba44c5dbfd57b68cb8ded6
Anastasia <vainer.lena@gmail.com>
2022-12-01 23:53:02 +0400

    done

--- Seminar001_hw1.2/Program.cs
+++ Seminar001_hw1.2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает
+//  на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Enter number1 : ");
+string enterNum1 = Console.ReadLine();
+
+Console.WriteLine("Enter number2 : ");
+string enterNum2 = Console.ReadLine();
+
+Console.WriteLine("Enter number3 : ");
+string enterNum3 = Console.ReadLine();
+
+int num1 = Convert.ToInt32(enterNum1);
+int num2 = Convert.ToInt32(enterNum2);
+int num3 = Convert.ToInt32(enterNum3);
+
+int max = num1;
+
+if(num1 > max) max = num1;
+if(num2 > max) max = num2;
+if(num3 > max) max = num3;
+
+Console.Write("max = ");
+Console.WriteLine(max);
