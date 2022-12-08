47f1fa7c7d258a5546454f4f53b8851db79981e6
Anastasia <vainer.lena@gmail.com>
2022-12-08 20:32:45 +0400

    done

---
+++ Example012_textKC/Program.cs
@@ -0,0 +1,41 @@
+﻿/* // Работа с текстом 
+Дан текст, в тексте, нужно все пробелы заменить черточками, маленькие
+буквы к, заменить большими К,
+а большие С, заменить маленькими с. */
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + " ежели бы вас послали вместо нашего милого Винценгероде,"
+            + " вы бы взяли приступом согласие прусского короля. "
+            + " Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012
+// s[3] // r
+// МЕТОД будет принимать строку и те символы, которые нужно будет 
+/* // менять, старый символ и на который нужно будет заменить
+Возвращаться будет строка
+4 вид методов
+ */
+         //   входеой текст   конкретный симв  символ, на который менять
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+
+    int length = text.Length;
+    for( int i = 0; i< length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+//затем выводим текст
+string newText = Replace(text, ' ', '|' );
+
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+
+Console.WriteLine();
+newText = Replace(newText, 'с', 'С');
+Console.WriteLine(newText);
---
+++ Example013_arraysort/Program.cs
@@ -0,0 +1,40 @@
+﻿/* 1 найти позицию минимального элемента в неотсортированной части массива
+2 произвести обмен этого значения со значением первой неотсортированной
+позиции
+3 повторять пока есть неотсортированные элементы */
+
+int[] arr = {1, 5, 3, 3, 2, 5, 6, 7, 2};
+
+void PrintArray(int[] array)  //вывести данный массив на экран
+{
+    int count = array.Length;
+    
+    for(int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+//метод, который будет упорядочивать массив
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i< array.Length; i++)
+    {
+        int minPosition = i; 
+        //ищем минимальный элемент
+        for(int j= i + 1; j < array.Length -1; j++)
+        {
+            if(array[j]< array[minPosition]) 
+            {
+                minPosition = j;
+            } 
+        }
+        int temp = array[i]; //поменять позицию с минимальной
+        array [i] = array[minPosition];
+        array[minPosition] = temp;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
