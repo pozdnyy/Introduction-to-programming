41aa7f222d4b7b5f6bde250b388653bb967b190c
Anastasia <vainer.lena@gmail.com>
2022-12-08 20:33:17 +0400

    done

--- Seminar003_hw3/Program.cs
+++ Seminar003_hw3/Program.cs
@@ -8,8 +8,10 @@
 
 Console.WriteLine("N : ");
 int N = Convert.ToInt32(Console.ReadLine());
-
-for (int i = 1; i <= N; i++)
+int i;
+for (i = 1; i <= N; i++)
 { 
     Console.Write($"{i*i*i} ");
 }
+
+//
