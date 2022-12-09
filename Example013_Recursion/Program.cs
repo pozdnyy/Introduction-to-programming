c7f3edee9ac6e84b68aa2640f9e269a3b2e02eae
Anastasia <vainer.lena@gmail.com>
2022-12-09 20:27:49 +0400

    lection 4

---
+++ Example013_Recursion/Program.cs
@@ -0,0 +1,44 @@
+﻿string[,] table = new string[2, 5];
+// String.Empty // инициализационная строка
+//table[0,0] table[0,1] table[0,2] table[0,4] 
+//table[1,0] table[1,1] table[1,2] table[1,4]
+// чтобы распечатать массив, нужно использовать цикл в цикле
+
+/* table[1,2] = "слово";// чтобы обратиться к нужному элементу , обращаемся к массиву
+for(int rows = 0; rows < 2; rows++) // rows - название счетчика
+{
+    for(int columns  = 0; columns <5; columns ++)
+    {
+        Console.WriteLine($"{table[rows,columns]}--");
+    }
+}
+ */
+////////////////////////////////////////////////
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10); // [1;10)
+        }
+    }
+}
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
---
+++ Example015_factorial/Program.cs
@@ -0,0 +1,41 @@
+﻿//метод, который принимает число, и возвращает факториал числа
+
+
+/* int Factorial (int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if(n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+Console.WriteLine(Factorial(3)); //1*2*3 = 6 */
+///////////////////////////////////////////////////////////
+// решение 40!
+/* double Factorial (int n) // тип данных double - потому что может хранить большие цифры в себе
+{
+    // 1! = 1
+    // 0! = 1
+    if(n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i< 40; i++)
+{
+Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+ */
+///////////////////////////////////////////////////////////
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+int Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for(int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
