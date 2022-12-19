cda15d2e09943c1545472763bf27fd23083d54a9
Anastasiia <vainer.lena@gmail.com>
2022-12-19 21:45:27 +0530

    done

--- Seminar007_hw2/Program.cs
+++ Seminar007_hw2/Program.cs
@@ -12,31 +12,33 @@
 
 17 -> такого числа в массиве нет */
 
-Console.WriteLine("Введите размеры массива");
-int m = Convert.ToInt32(Console.ReadLine());
-int n = Convert.ToInt32(Console.ReadLine());
-int[,] array = new int[m, n];
- 
-for (int i = 0; i < array.GetLength(0); i++) 
+Console.WriteLine("Enter size:");
+
+int q = Convert.ToInt32(Console.ReadLine());
+int g = Convert.ToInt32(Console.ReadLine());
+
+int[,] collection = new int[q, g];
+
+for (int i = 0; i < collection.GetLength(0); i++)
 {
-    for (int j = 0; j < array.GetLength(1); j++)
-        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
+    for (int j = 0; j < collection.GetLength(1); j++)
+        collection[i, j] = Convert.ToInt32(new Random().Next(0, 21));
 }
- 
-for (int i = 0; i < array.GetLength(0); i++) 
+
+for (int i = 0; i < collection.GetLength(0); i++)
 {
-    for (int j = 0; j < array.GetLength(1); j++)
-        Console.Write(array[i,j] + "\t  ");
-        Console.WriteLine();
+    for (int j = 0; j < collection.GetLength(1); j++)
+        Console.Write(collection[i, j] + "\t  ");
+    Console.WriteLine();
 }
- 
- Console.WriteLine("Введите координаты");
- int a = Convert.ToInt32(Console.ReadLine());
- int b = Convert.ToInt32(Console.ReadLine());
- if (a>m && b>n)
- Console.WriteLine("такого числа нет");
- else
- {
- object c = array.GetValue(a,b);
- Console.WriteLine(c);
- }
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > q && b > g)
+    Console.WriteLine("такого числа нет");
+else
+{
+    object c = collection.GetValue(a, b);
+    Console.WriteLine(c);
+}
