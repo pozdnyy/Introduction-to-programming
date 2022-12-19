f7c0f57f2fbf6796506eff049237e52c825d9507
Anastasiia <vainer.lena@gmail.com>
2022-12-19 21:31:53 +0530

    done

---
+++ Seminar007_hw1/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Задача 47. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+
+1 -3,3 8 -9,9
+
+8 7,8 -7,1 9 */
+
+Console.WriteLine("Enter the number of lines: ");
+int line = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number of columns");
+int column = Convert.ToInt32(Console.ReadLine());
+
+double[,] collection = new double[line, column];
+
+FillRandomnums(collection);
+PrintArray(collection);
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write(" ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write(" ");
+        Console.WriteLine("");
+    }
+}
+
+void FillRandomnums(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
